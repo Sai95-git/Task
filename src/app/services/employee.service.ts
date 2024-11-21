88e5062f35d6a5f9a67ab98a8352b2a0aeaf87fd
Sai95-git <madipeddi.saimanikanta@geeth.ai>
2024-11-21 16:40:44 +0530

    initial commit

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -20,6 +20,7 @@ export class EmployeeService {
     return this._http.get('http://localhost:3000/employees');
   }
 
+  
   deleteEmployee(id: number): Observable<any> {
     return this._http.delete(`http://localhost:3000/employees/${id}`);
   }
